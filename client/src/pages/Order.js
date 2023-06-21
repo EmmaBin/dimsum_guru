554ac48b46cccdf1246ea1d09fa36917ecbf8116
emmacodes <binma.emma@gmail.com>
2023-06-21 14:53:47 -0700

    added on-hover displaying btn effect

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -25,14 +25,16 @@ export default function Order() {
             {foods.map((food) => {
                 return (
 
-                    <div key={food.food_id} className="w-full md:w-4/5" >
+                    <div key={food.food_id} className="w-full md:w-4/5 group" >
                         <div className="w-full h-48 md:h-64 relative overflow-hidden">
-                            <img src={`http://localhost:5000/${food.image}`} alt={food.name} className="absolute top-0 left-0 w-full h-full object-cover" />
+                            <img src={`http://localhost:5000/${food.image}`} alt={food.name} className="absolute top-0 left-0 w-full h-full object-cover rounded-md hover:blur-sm" />
+                            <button className="hidden group-hover:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-teal-500 p-2 rounded-md px-8 text-white">ADD TO CART</button>
                         </div>
                         <div className="flex justify-between text-slate-950">
                             <h4>{food.name.toUpperCase()}</h4>
                             <h4>{food.price}</h4>
                         </div>
+
                     </div>)
 
             })}
