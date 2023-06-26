841646566fdd8e5fa8eb087be2878ac5d3ff9fd3
emmacodes <binma.emma@gmail.com>
2023-06-26 12:35:16 -0700

    looped over cart

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -6,11 +6,11 @@ export default function Checkout({ cart, setCart, total }) {
         <div className="bg-blue-200">
             Here is the cart info
             {cart.map((item) => (
-                <div key={item.food_id}>
-                    <p>{item.food_id}</p>
+                <div key={item.food_id} className="flex">
+                    <img src={`http://localhost:5000/${item.image}`} alt={item.name} className="w-20 h-20"/>
                     <p>{item.name}</p>
                     <p>{item.price}</p>
-                    <img src={item.image} alt={item.name} />
+                    
                 </div>
             ))}
             <p>Here is the total price: {total}</p>
