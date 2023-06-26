cde4a263e02ef4a97370262d6dfa675db7fd3b5d
emmacodes <binma.emma@gmail.com>
2023-06-26 14:10:55 -0700

    display overall layout of shopping cart

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function Admin() {
     return (
     <>
-        <h1 className=" bg-blue-200">This is Admin page to have CRUD functionalities</h1>
+        <h1 className=" bg-blue-200">Admin page has the functionality to view total revenue, all existing menu, and CRUD functions</h1>
     </>
     )
 
--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -3,19 +3,38 @@ import React, { useEffect, useState } from "react";
 export default function Checkout({ cart, setCart, total }) {
 
     return (
-        <div className="bg-blue-200">
-            Here is the cart info
-            {cart.map((item) => (
-                <div key={item.food_id} className="flex">
-                    <img src={`http://localhost:5000/${item.image}`} alt={item.name} className="w-20 h-20"/>
-                    <p>{item.name}</p>
-                    <p>{item.price}</p>
-                    
-                </div>
-            ))}
-            <p>Here is the total price: {total}</p>
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <div className="flow-root">
+                    Here is the cart info
+                    <ul className="divide-y divide-gray-200 dark:divide-gray-700">
 
+
+                        {cart.map((item) => (
+                            <li key={item.food_id} className="py-3 sm:py-4">
+                                <div class="flex items-center space-x-4">
+                                    <div className="flex-shrink-0">
+                                        <img src={`http://localhost:5000/${item.image}`} alt={item.name} className="w-20 h-20" />
+                                    </div>
+                                    <div className="flex-1 min-w-0">
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            {item.name}
+                                        </p>
+
+                                    </div>
+                                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                        <p>${item.price}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <p>Here is the total price: {total}</p>
+
+                </div>
+            </div>
         </div>
     )
 
 }
+
